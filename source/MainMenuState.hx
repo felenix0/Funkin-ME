0a3b654ba26b72a261b68edea806fe17e38fc39e
felenix0 <bizzarifoxy21@gmail.com>
2022-05-01 17:39:35 -0300

    changed Mode of Engine's Version

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -96,7 +96,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollow, null, 0.06);
 
-		var engineVersionShit:FlxText = new FlxText(5, FlxG.height - 38, 0, "MercuryEngine' v" + "0.0.5", 12);
+		var engineVersionShit:FlxText = new FlxText(5, FlxG.height - 38, 0, "MercuryEngine' v" + Application.current.meta.get('versionEngine'), 12);
 		engineVersionShit.scrollFactor.set();
 		engineVersionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(engineVersionShit);
